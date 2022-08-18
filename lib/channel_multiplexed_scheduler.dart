e0bac4e98e3a2a2565bef35ca47e29fc510fcdb0
Rémy Raes <contact@remyraes.com>
2022-08-18 13:47:58 +0200

    build: remove plugin-related files

--- lib/channel_multiplexed_scheduler.dart
+++
@@ -1,7 +0,0 @@
-import 'channel_multiplexed_scheduler_platform_interface.dart';
-
-class ChannelMultiplexedScheduler {
-  Future<String?> getPlatformVersion() {
-    return ChannelMultiplexedSchedulerPlatform.instance.getPlatformVersion();
-  }
-}
