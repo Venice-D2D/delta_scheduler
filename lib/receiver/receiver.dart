cbabc2c4b6369b991436d3fba313c5f9c28b271b
Remy Raes <contact@remyraes.com>
2022-08-22 22:07:48 +0200

    feat: make receiver not abstract

--- lib/receiver/receiver.dart
+++ lib/receiver/receiver.dart
@@ -1,12 +1,12 @@
 import 'package:channel_multiplexed_scheduler/channels/channel.dart';
 import 'package:flutter/material.dart';
 
-abstract class Receiver {
+class Receiver {
   late final List<Channel> _channels = [];
 
   /// Adds a channel to use to receive data.
-  void useChannel(Channel channel);
+  void useChannel(Channel channel) {}
 
   /// Receives a file through available channels.
-  Future<void> receiveFile(Path destination);
+  Future<void> receiveFile(Path destination) async {}
 }
