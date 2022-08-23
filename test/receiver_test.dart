ed720d53c5c475db1a9f9ceeb4ae3e2e65dce038
Remy Raes <contact@remyraes.com>
2022-08-23 08:29:00 +0200

    test: receiver should init channels

--- test/receiver_test.dart
+++ test/receiver_test.dart
@@ -2,6 +2,8 @@ import 'package:channel_multiplexed_scheduler/receiver/receiver.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'mock/channel/mock_channel.dart';
+
 void main() {
   late Receiver receiver;
 
@@ -13,5 +15,17 @@ void main() {
     expect(() async => await receiver.receiveFile(Path()),
           throwsA(predicate((e) => e is StateError
               && e.message == 'Cannot receive file because receiver has no channel.')));
+  });
+
+  test('should init channels', () async {
+    MockChannel channel1 = MockChannel();
+    MockChannel channel2 = MockChannel();
+    receiver.useChannel(channel1);
+    receiver.useChannel(channel2);
+
+    await receiver.receiveFile(Path());
+
+    expect(channel1.isInit, true);
+    expect(channel2.isInit, true);
   });
 }
