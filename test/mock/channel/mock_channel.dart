37cbb1548cb037e6adb0ba235a5ac27b2ebad852
Rémy Raes <contact@remyraes.com>
2022-08-25 13:41:21 +0200

    test: sendFile should send all chunks even if some are dropped

--- test/mock/channel/mock_channel.dart
+++ test/mock/channel/mock_channel.dart
@@ -3,13 +3,18 @@ import 'dart:math';
 import 'package:channel_multiplexed_scheduler/channels/channel.dart';
 import 'package:channel_multiplexed_scheduler/channels/channel_event.dart';
 import 'package:channel_multiplexed_scheduler/file/file_chunk.dart';
+import 'package:flutter/material.dart';
 
 
 /// This is a mock Channel implementation that's used in tests.
 class MockChannel extends Channel {
+  bool shouldDropChunks;
   bool isInitSender = false;
   bool isInitReceiver = false;
   List<int> sentChunksIds = [];
+
+  MockChannel({this.shouldDropChunks = false});
+
 
   @override
   Future<void> initSender() async {
@@ -23,9 +28,17 @@ class MockChannel extends Channel {
 
   /// Stupid chunk sending emulation, that acknowledges a chunk after a random
   /// delay to simulate network latency.
+  /// If shouldDropChunks is set to true, this will simulate packet drop from
+  /// time to time, not sending any acknowledgement at all.
   @override
   Future<void> sendChunk(FileChunk chunk) async {
-    await Future.delayed(Duration(milliseconds: Random().nextInt(2000)), () {
+    Random r = Random();
+    if (shouldDropChunks && r.nextBool()) {
+      debugPrint('==> [MockChannel] Dropped chunk n°${chunk.identifier}.');
+      return;
+    }
+
+    await Future.delayed(Duration(milliseconds: r.nextInt(2000)), () {
       if (!sentChunksIds.contains(chunk.identifier)) {
         sentChunksIds.add(chunk.identifier);
       }
--- test/scheduler_test.dart
+++ test/scheduler_test.dart
@@ -104,5 +104,17 @@ void main() {
       expect(channel1.sentChunksIds, [0, 1, 2, 3, 4, 5, 6, 7, 8]);
       expect(channel2.sentChunksIds.isEmpty, true);
     });
+
+    test('should send all chunks even if some are dropped', () async {
+      MockChannel channel = MockChannel(shouldDropChunks: true);
+
+      scheduler.useChannel(channel);
+      await scheduler.sendFile(file, 100000);
+
+      // chunks are not sent in order, so we need to sort their ids
+      channel.sentChunksIds.sort((int a, int b) => a - b);
+
+      expect(channel.sentChunksIds, [0, 1, 2, 3, 4, 5, 6, 7, 8]);
+    });
   });
 }
