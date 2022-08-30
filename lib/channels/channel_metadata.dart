0e7b27c6061f0549cfbe28db037f134472bc965b
Rémy Raes <contact@remyraes.com>
2022-08-30 15:37:30 +0200

    feat: add metadata payload classes

---
+++ lib/channels/channel_metadata.dart
@@ -0,0 +1,7 @@
+class ChannelMetadata {
+  final String address;
+  final String identifier;
+  final String password;
+
+  ChannelMetadata(this.address, this.identifier, this.password);
+}
---
+++ lib/file/file_metadata.dart
@@ -0,0 +1,7 @@
+class FileMetadata {
+  final String name;
+  final int chunkSize;
+  final int chunkCount;
+
+  FileMetadata(this.name, this.chunkSize, this.chunkCount);
+}
