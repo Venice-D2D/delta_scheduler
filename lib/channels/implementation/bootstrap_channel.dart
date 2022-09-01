4144ca810d1353b7102b5a2463d318259e7a42de
Remy Raes <raes.remy@gmail.com>
2022-09-01 12:33:21 +0200

    fix: BootstrapChannel extends Channel

--- lib/channels/implementation/bootstrap_channel.dart
+++ lib/channels/implementation/bootstrap_channel.dart
@@ -1,8 +1,9 @@
+import 'package:channel_multiplexed_scheduler/channels/channel.dart';
 import 'package:channel_multiplexed_scheduler/channels/channel_metadata.dart';
 import 'package:channel_multiplexed_scheduler/channels/events/bootstrap_channel_event.dart';
 import 'package:channel_multiplexed_scheduler/file/file_metadata.dart';
 
-abstract class BootstrapChannel {
+abstract class BootstrapChannel extends Channel {
   late Function(BootstrapChannelEvent event, dynamic data) on;
   Future<void> sendFileMetadata(FileMetadata data);
   Future<void> sendChannelMetadata(ChannelMetadata data);
