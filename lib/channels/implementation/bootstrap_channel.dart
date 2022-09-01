5a103685b379e36547d360d6cf457b090c4dccc8
Remy Raes <raes.remy@gmail.com>
2022-09-01 12:31:07 +0200

    fix: typo on BootstrapChannel name

--- lib/channels/implementation/bootstrap_channel.dart
+++ lib/channels/implementation/bootstrap_channel.dart
@@ -2,7 +2,7 @@ import 'package:channel_multiplexed_scheduler/channels/channel_metadata.dart';
 import 'package:channel_multiplexed_scheduler/channels/events/bootstrap_channel_event.dart';
 import 'package:channel_multiplexed_scheduler/file/file_metadata.dart';
 
-abstract class BoostrapChannel {
+abstract class BootstrapChannel {
   late Function(BootstrapChannelEvent event, dynamic data) on;
   Future<void> sendFileMetadata(FileMetadata data);
   Future<void> sendChannelMetadata(ChannelMetadata data);
