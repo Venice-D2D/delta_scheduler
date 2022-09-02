47979560859aeccd05f8c6a99f1fff27ddf48631
Rémy Raes <contact@remyraes.com>
2022-09-02 13:30:27 +0200

    feat: ChannelMetadata instances can be compared

--- lib/channels/channel_metadata.dart
+++ lib/channels/channel_metadata.dart
@@ -9,4 +9,12 @@ class ChannelMetadata {
   String toString() {
     return "$address;$identifier;$password";
   }
+
+  @override
+  bool operator ==(Object other) {
+    return other is ChannelMetadata && address == other.address && identifier == other.identifier && password == other.password;
+  }
+
+  @override
+  int get hashCode => int.parse("${address.hashCode}${identifier.hashCode}${password.hashCode}");
 }
