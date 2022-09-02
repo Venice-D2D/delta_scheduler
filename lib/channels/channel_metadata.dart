c98c0abd178538e000f22d23b6a5ec25d544ac91
Rémy Raes <contact@remyraes.com>
2022-09-02 10:37:49 +0200

    feat: override toString method in metadata payloads

--- lib/channels/channel_metadata.dart
+++ lib/channels/channel_metadata.dart
@@ -4,4 +4,9 @@ class ChannelMetadata {
   final String password;
 
   ChannelMetadata(this.address, this.identifier, this.password);
+
+  @override
+  String toString() {
+    return "$address;$identifier;$password";
+  }
 }
--- lib/file/file_metadata.dart
+++ lib/file/file_metadata.dart
@@ -4,4 +4,9 @@ class FileMetadata {
   final int chunkCount;
 
   FileMetadata(this.name, this.chunkSize, this.chunkCount);
+
+  @override
+  String toString() {
+    return "$name;$chunkSize;$chunkCount";
+  }
 }
