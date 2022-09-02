f398f648767876d8211daf7a52c72c99187198ea
Rémy Raes <contact@remyraes.com>
2022-09-02 10:47:28 +0200

    test: add FileBootstrapChannel._createMockPacket helper method

--- test/mock/channel/file_bootstrap_channel.dart
+++ test/mock/channel/file_bootstrap_channel.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:channel_multiplexed_scheduler/channels/channel_metadata.dart';
 import 'package:channel_multiplexed_scheduler/channels/implementation/bootstrap_channel.dart';
 import 'package:channel_multiplexed_scheduler/file/file_metadata.dart';
+import 'package:flutter/material.dart';
 
 class FileBootstrapChannel extends BootstrapChannel {
   // This directory will store all package exchanged between sender and receiver.
@@ -24,11 +26,27 @@ class FileBootstrapChannel extends BootstrapChannel {
 
   @override
   Future<void> sendChannelMetadata(ChannelMetadata data) async {
-    // TODO create file with metadata
+    _createMockPacket(data, true);
   }
 
   @override
   Future<void> sendFileMetadata(FileMetadata data) async {
-    // TODO create file with metadata
+    _createMockPacket(data, false);
+  }
+
+  /// This helper function creates a file on watched directory, representing a
+  /// packet sent over network.
+  Future<void> _createMockPacket(dynamic content, bool isChannelMetadata) async {
+    if (content is! ChannelMetadata && content is! FileMetadata) {
+      throw StateError("Tried to send mock packet with incorrect data type.");
+    }
+
+    File packetFile = File(directory.path + Platform.pathSeparator + UniqueKey().toString());
+    await packetFile.create();
+
+    String packetContent = content.toString();
+    String finalContent = "${content is ChannelMetadata ? "c" : "f"};$packetContent";
+
+    await packetFile.writeAsBytes(utf8.encode(finalContent));
   }
 }
