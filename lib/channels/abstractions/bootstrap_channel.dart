08c484c850f421bada19d16d0f8092eb1cb73da8
Rémy Raes <contact@remyraes.com>
2022-09-12 16:37:59 +0200

    docs: update BootstrapChannel documentation

--- lib/channels/abstractions/bootstrap_channel.dart
+++ lib/channels/abstractions/bootstrap_channel.dart
@@ -3,13 +3,20 @@ import 'package:channel_multiplexed_scheduler/channels/events/bootstrap_channel_
 import 'package:channel_multiplexed_scheduler/file/file_metadata.dart';
 
 abstract class BootstrapChannel {
+  /// Provides information to sending and receiving ends about what's happening
+  /// in the current channel.
   late Function(BootstrapChannelEvent event, dynamic data) on;
+
   /// Initializes current channel, and returns when it is ready to send data.
   Future<void> initSender();
 
   /// Initializes current channel, and returns when it is ready to receive data.
   Future<void> initReceiver();
 
+  /// Sends file metadata to receiving end.
   Future<void> sendFileMetadata(FileMetadata data);
+
+  /// Sends channel metadata to receiving end, for it to initialize data
+  /// channels before starting file exchange.
   Future<void> sendChannelMetadata(ChannelMetadata data);
 }
