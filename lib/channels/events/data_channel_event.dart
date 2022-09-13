8e720a37b62c8c3d2cf4dd715571883fdb5371ee
Rémy Raes <contact@remyraes.com>
2022-09-13 16:21:17 +0200

    docs: add documentation to event enumerations

--- lib/channels/events/data_channel_event.dart
+++ lib/channels/events/data_channel_event.dart
@@ -1,4 +1,10 @@
+/// These events will be fired on data channels, to indicate the type of data
+/// that was received.
 enum DataChannelEvent {
+  /// Indicates that payload is the identifier of the file chunk that was
+  /// acknowledged.
   acknowledgment,
+
+  /// Indicates that payload is of FileChunk type.
   data
 }
