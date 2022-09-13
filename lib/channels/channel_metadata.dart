e45297517c31ef0f21b217cc0d4d7d36e494003d
Rémy Raes <contact@remyraes.com>
2022-09-13 16:35:58 +0200

    docs: add documentation to metadata classes

--- lib/channels/channel_metadata.dart
+++ lib/channels/channel_metadata.dart
@@ -1,10 +1,23 @@
+/// This payload class holds data needed for a channel's receiving end to
+/// connect to sending end; as such, it is sent from sender part to receiver
+/// through a bootstrap channel.
 class ChannelMetadata {
+  /// Identifies in a unique way the channel concerned by this instance.
+  /// Must have the same value on both sending and receiving ends.
   final String channelIdentifier;
+
+  /// Physical address of the server socket.
   final String address;
+
+  /// Name of the access point (needed for Wi-Fi, for instance).
   final String apIdentifier;
+
+  /// Password needed to connect to the distant access point.
   final String password;
 
-  ChannelMetadata(this.channelIdentifier, this.address, this.apIdentifier, this.password);
+  ChannelMetadata(
+      this.channelIdentifier, this.address, this.apIdentifier, this.password);
+
 
   @override
   String toString() {
@@ -12,10 +25,15 @@ class ChannelMetadata {
   }
 
   @override
-  bool operator ==(Object other) {
-    return other is ChannelMetadata && channelIdentifier == other.channelIdentifier && address == other.address && apIdentifier == other.apIdentifier && password == other.password;
+  bool operator == (Object other) {
+    return other is ChannelMetadata
+        && channelIdentifier == other.channelIdentifier
+        && address == other.address
+        && apIdentifier == other.apIdentifier
+        && password == other.password;
   }
 
   @override
-  int get hashCode => Object.hash(channelIdentifier, address, apIdentifier, password);
+  int get hashCode => Object.hash(
+      channelIdentifier, address, apIdentifier, password);
 }
--- lib/file/file_metadata.dart
+++ lib/file/file_metadata.dart
@@ -1,9 +1,19 @@
+/// This payload class holds data needed for the receiving device to initialize
+/// primitives related to the file that will be sent once all data channels are
+/// ready.
+/// This is typically sent from sender to receiver through a bootstrap channel.
 class FileMetadata {
+  /// Name of the file to be transferred.
   final String name;
+
+  /// Size of a file chunk.
   final int chunkSize;
+
+  /// Number of file chunks.
   final int chunkCount;
 
   FileMetadata(this.name, this.chunkSize, this.chunkCount);
+
 
   @override
   String toString() {
@@ -11,8 +21,11 @@ class FileMetadata {
   }
 
   @override
-  bool operator ==(Object other) {
-    return other is FileMetadata && name == other.name && chunkSize == other.chunkSize && chunkCount == other.chunkCount;
+  bool operator == (Object other) {
+    return other is FileMetadata
+        && name == other.name
+        && chunkSize == other.chunkSize
+        && chunkCount == other.chunkCount;
   }
 
   @override
